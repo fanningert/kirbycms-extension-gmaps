7d24b2aa64407b1b3c9aa2174fc0679e3e109018
Thomas Fanninger <thomas@fanninger.at>
2015-05-26 22:55:28 +0200

    Bugfix

--- assets/js/gmaps.js
+++ assets/js/gmaps.js
@@ -4,8 +4,8 @@ function initialize() {
     var lat = jQuery(this).attr('data-lat');
     var lng = jQuery(this).attr('data-lng');
     var zoom = parseInt(jQuery(this).attr('data-zoom'));
-    var disable_zoom = jQuery.parseJSON(jQuery(this).attr('data-ui-zoom'));
-    var disable_draggable = jQuery.parseJSON(jQuery(this).attr('data-ui-draggable'));
+    var ui_zoom = jQuery.parseJSON(jQuery(this).attr('data-ui-zoom'));
+    var ui_draggable = jQuery.parseJSON(jQuery(this).attr('data-ui-draggable'));
     var kml = jQuery(this).attr('data-kml');
     var marker = jQuery(this).attr('data-marker');
     
@@ -13,10 +13,10 @@ function initialize() {
     var mapOptions = new Array();
     mapOptions.zoom = zoom;
     mapOptions.center = myLatLng;
-    if (disable_zoom){
+    if (!ui_draggable){
       mapOptions.draggable = false;
     }
-    if (disable_zoom) {
+    if (!ui_zoom) {
       mapOptions.zoomControl = false;
       mapOptions.scrollwheel = false;
       mapOptions.disableDoubleClickZoom = true;
