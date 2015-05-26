b8c62e3bef8cc0cd6bab036e23e6ede66e284e3f
Thomas Fanninger <thomas@fanninger.at>
2015-05-26 22:27:16 +0200

    Bugfix

--- assets/js/gmaps.js
+++ assets/js/gmaps.js
@@ -4,13 +4,13 @@ function initialize() {
     var lat = jQuery(this).attr('data-lat');
     var lng = jQuery(this).attr('data-lng');
     var zoom = parseInt(jQuery(this).attr('data-zoom'));
-    var disable_zoom = (jQuery(this).attr('data-disable-zoom').toLowerCase() === 'true')
-    var disable_draggable = (jQuery(this).attr('data-disable-draggable').toLowerCase() === 'true')
+    var disable_zoom = (jQuery(this).attr('data-disable-zoom') === 'true')
+    var disable_draggable = (jQuery(this).attr('data-disable-draggable') === 'true')
     var kml = jQuery(this).attr('data-kml');
     var marker = jQuery(this).attr('data-marker');
     
     var myLatLng = new google.maps.LatLng(lat, lng);
-    var mapOptions = new google.maps.MapOptions();
+    var mapOptions = new Array();
     mapOptions.zoom = zoom;
     mapOptions.center = myLatLng;
     if (disable_zoom){
